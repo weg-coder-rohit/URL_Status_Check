0b093cbc50b919a836dc9a61a890435b77e8b635
weg-coder-rohit <rohit.singh042499@gmail.com>
2024-06-25 21:41:22 +0530

    removed sensitive info

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "url_checker" {
   source = "./module/url_checker"
 
-  sns_email_subscription = "rohit.singh042499@gmail.com"
+  sns_email_subscription = "YourEmail@example.com"
   lambda_function_name   = "url-checker-lambda"
 }
